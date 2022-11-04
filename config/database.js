1fa82007cc3b2b917fe9576d2786601de3aca3b6
FARHAN <developer@pitjarus.com>
2022-11-04 21:26:18 +0700

    change create table query and schema

--- config/database.js
+++ config/database.js
@@ -12,40 +12,55 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
         throw err
     }else{
         console.log('Connected to the SQLite database.')
+        const createdAt = moment().format("YYYY-MM-DD HH:mm:ss")
         db.run(`CREATE TABLE user (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name text, 
             username text UNIQUE, 
             password text, 
-            balance DOUBLE,
             createdAt text,
             updatedAt text,
-            CONSTRAINT username_unique UNIQUE (username));
-            CREATE TABLE transaction (
+            CONSTRAINT username_unique UNIQUE (username));`,
+            async (err) => {
+                if (err) {
+                    // Table already created
+                }else{
+                    // Table just created, creating some rows
+                    let insert = 'INSERT INTO user (name, username, password,createdAt,updatedAt) VALUES (?,?,?,?,?)'
+                    const password = await generatePasswordHash('@dminTest123')
+                    db.run(insert, ["admin","admin",password,createdAt,''])
+                }
+            });
+        db.run(`CREATE TABLE transactionType (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            transactionType text,
+            isDeduct BOOLEAN,
+            createdAt text,
+            updatedAt text);`,
+            async (err) => {
+                if (err) {
+                    // Table already created
+                }else{
+                    // Table just created, creating some rows
+                    let insert = 'insert into transactionType(transactionType,isDeduct,createdAt,updatedAt)  VALUES (?,?,?,?)'
+                    db.run(insert, ['income',0,createdAt,''])
+                    db.run(insert, ['expenses',1,createdAt,''])
+                }
+            });
+        db.run(`CREATE TABLE transactions (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             userId INTEGER,
             transactionTypeId INTEGER,
             transactionValue DOUBLE,
             transactionDesc text, 
             createdAt text, 
-            updatedAt text);
-            CREATE TABLE transactionType (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            transactionType text,
-            isDeduct BOOLEAN
-            createdAt text, 
-            createdBy INTEGER, 
-            updatedAt text,
-            updatedBy INTEGER);`,
+            updatedAt text);`,
             async (err) => {
                 if (err) {
                     // Table already created
                 }else{
-                    // Table just created, creating some rows
-                    const insert = 'INSERT INTO user (name, username, password,balance,createdAt,updatedAt) VALUES (?,?,?,?,?,?)'
-                    const password = await generatePasswordHash('@dminTest123');
-                    const createdAt = moment().format("YYYY-MM-DD HH:mm:ss");
-                    db.run(insert, ["admin","admin",password,10000,createdAt,''])
+                    let insert = 'insert into transactions(userId,transactionTypeId,transactionValue,transactionDesc,createdAt,updatedAt)  VALUES (?,?,?,?,?,?)'
+                    db.run(insert, [1,1,10000,'Initial Balance',createdAt,''])
                 }
             });
     }
