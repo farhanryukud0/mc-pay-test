ef9078050296abd1adb6b19be32c27f56330fab7
FARHAN <developer@pitjarus.com>
2022-11-04 21:26:44 +0700

    add transaction for insert user

--- controller/user.js
+++ controller/user.js
@@ -1,36 +1,47 @@
 const catchAsync = require('../util/catchAsync')
 const {defaultResponse, failResponse, messageResponse} = require('../util/defaultResponse')
 const db = require("../config/database.js")
-const {generatePasswordHash} = require("../util/hashPassword");
-const moment = require("moment-timezone");
-const {StatusCodes} = require("http-status-codes");
+const {generatePasswordHash} = require("../util/hashPassword")
+const moment = require("moment-timezone")
+const {StatusCodes} = require("http-status-codes")
 
 const getUsers = catchAsync(async (req, res) => {
     const sql = "select * from user"
     const params = []
-    db.all(sql, params, (err, rows) => {
+    db.all(sql, params, function (err, rows) {
         if (err) {
-            return messageResponse(res, err.message);
+            return messageResponse(res, err.message)
         }
-        return defaultResponse(res,rows);
-    });
-});
+        return defaultResponse(res,rows)
+    })
+})
 
 const createUser = catchAsync(async (req, res) => {
-    const sql = 'INSERT INTO user (name, username, password,balance,createdAt,updatedAt) VALUES (?,?,?,?,?,?)'
+    let sql = 'INSERT INTO user (name, username, password,createdAt,updatedAt) VALUES (?,?,?,?,?)'
     const param = req.body
     const password = await generatePasswordHash(param.password)
     const createdAt = moment().format("YYYY-MM-DD HH:mm:ss")
-    const params = [param.name,param.username,password,param.balance,createdAt,'']
-    db.run(sql, params, (err, rows) => {
+    let params = [param.name,param.username,password,createdAt,'']
+    db.run('begin transaction')
+    db.run(sql, params, function (err, row) {
         if (err) {
-            return messageResponse(res, err.message);
+            db.run('rollback')
+            return messageResponse(res, err.message)
         }
-        return messageResponse(res,'Data berhasil terinput',StatusCodes.OK);
-    });
-});
+        sql = 'INSERT INTO transactions (userId,transactionTypeId,transactionValue,transactionDesc,createdAt,updatedAt)  VALUES (?,?,?,?,?,?)'
+        params = [this.lastID,1,param.balance,'Initial Balance',createdAt,'']
+        db.run(sql, params, function (err, row) {
+            if (err) {
+                db.run('rollback')
+                return messageResponse(res, err.message)
+            }
+            db.run('commit')
+            return messageResponse(res,'Data berhasil terinput',StatusCodes.OK)
+        })
+    })
+})
 
 module.exports = {
     getUsers,
     createUser
-};
+}
